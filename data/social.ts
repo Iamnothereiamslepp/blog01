ecfacc625e8844f475690465d9dacc0a5f967080
Iamnothereiamslepp <128118699+Iamnothereiamslepp@users.noreply.github.com>
2024-11-17 22:17:31 +0800

    Update social.ts

--- data/social.ts
+++ data/social.ts
@@ -18,14 +18,14 @@ type SocialValue = {
 }
 
 const social: Social = {
-  github: 'https://github.com/kuizuo',
+  github: 'https://github.com/ragroup01',
   x: 'https://twitter.com/kuizuo',
   juejin: 'https://juejin.cn/user/1565318510545901',
   wx: 'https://img.kuizuo.cn/wechat.png',
   // qq: 'https://img.kuizuo.cn/qq.png',
   // zhihu: 'https://www.zhihu.com/people/kuizuo',
   cloudmusic: 'https://music.163.com/#/user/home?id=1333010742',
-  email: 'mailto:hi@kuizuo.cn',
+  email: 'mailto:ralivemailme@gmail.com',
   discord: 'https://discord.gg/M8cVcjDxkz',
 }
 
