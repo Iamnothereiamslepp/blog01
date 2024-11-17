0e85b7cfb8bbc57a859ae9eaf772b4e37a0508bd
Iamnothereiamslepp <128118699+Iamnothereiamslepp@users.noreply.github.com>
2024-11-17 22:04:12 +0800

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -22,10 +22,10 @@ const config: Config = {
       '是一个由RALive的主頁和博客，主要分享编程开发知识和项目，该网站基于 React 驱动的静态网站生成器 Docusaurus 构建。',
   },
   themeConfig: {
-    // announcementBar: {
-    //   id: 'announcementBar-3',
-    //   content: `announcementBarContent`,
-    // },
+     announcementBar: {
+       id: 'announcementBar-3',
+       content: `announcementBarContent`,
+     },
     image: 'img/og.png',
     metadata: [
       {
