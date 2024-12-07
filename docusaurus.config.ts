ccc99496f9096d5bd93182c745e7ce3dd9ae29d7
Iamnothereiamslepp <128118699+Iamnothereiamslepp@users.noreply.github.com>
2024-12-08 00:36:36 +0800

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -300,7 +300,7 @@ Love what you do and do what you love.
   ],
   i18n: {
     defaultLocale: 'zh-hant',
-    locales: ['zh-hant', 'zh-Hant-HK', 'zho', 'xgng', 'zh-tw'],
+    locales: ['zh-hant', 'zh-Hant-HK', 'zho', 'zh-tw'],
   },
   onBrokenLinks: 'warn',
 }
