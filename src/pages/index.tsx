a456232acb21e991f1660c78dae91940c07e4940
Iamnothereiamslepp <128118699+Iamnothereiamslepp@users.noreply.github.com>
2024-12-09 21:07:02 +0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,10 +21,9 @@ export default function Home() {
 
         <div className="relative">
           <div className="mx-auto max-w-7xl bg-background lg:px-8">
-            <BlogSection />
             <ProjectSection />
+            <BlogSection />
             <FeaturesSection />
-            <Companies />
           </div>
           <div
             className="-z-50 absolute inset-0 bg-grid-slate-50 [mask-image:linear-gradient(0deg,#fff,rgba(255,255,255,0.3))] dark:bg-grid-slate-700/25 dark:[mask-image:linear-gradient(0deg,rgba(255,255,255,0.1),rgba(255,255,255,0.5))]"
