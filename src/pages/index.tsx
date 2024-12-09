f5152e65ef43c0b03e84efb3bd6a94b7b7b39424
Iamnothereiamslepp <128118699+Iamnothereiamslepp@users.noreply.github.com>
2024-12-09 21:02:51 +0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import FeaturesSection from '../components/landing/FeaturesSection'
 import Hero from '../components/landing/Hero'
 import ProjectSection from '../components/landing/ProjectSection'
 import Particles from '../components/magicui/particles'
+import Companies from '../components/landing/Companysections'
 
 export default function Home() {
   const {
@@ -20,9 +21,10 @@ export default function Home() {
 
         <div className="relative">
           <div className="mx-auto max-w-7xl bg-background lg:px-8">
-            <BlogSection />
+            <Companies />
             <ProjectSection />
             <FeaturesSection />
+            <BlogSection />
           </div>
           <div
             className="-z-50 absolute inset-0 bg-grid-slate-50 [mask-image:linear-gradient(0deg,#fff,rgba(255,255,255,0.3))] dark:bg-grid-slate-700/25 dark:[mask-image:linear-gradient(0deg,rgba(255,255,255,0.1),rgba(255,255,255,0.5))]"
