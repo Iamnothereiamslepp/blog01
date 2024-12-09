2b5ab74e6aaa7d3d0d5d22cb2d42067f0b95179f
Iamnothereiamslepp <128118699+Iamnothereiamslepp@users.noreply.github.com>
2024-12-09 22:40:26 +0800

    Update index.tsx

--- src/components/landing/CompanySections/index.tsx
+++ src/components/landing/CompanySections/index.tsx
@@ -1,42 +1,29 @@
 import Translate from '@docusaurus/Translate'
+import features from '@site/data/features'
 import { cn } from '@site/src/lib/utils'
 import { BentoGrid, BentoGridItem } from '../../magicui/bento-grid'
 import { Section } from '../Section'
 
-const companies = [
-  "Google",
-  "Microsoft",
-  "Amazon",
-  "Netflix",
-  "YouTube",
-  "Instagram",
-  "Uber",
-  "Spotify",
-];
-
-export default function Companies() {
+export default function FeaturesSection() {
   return (
     <Section title={<Translate id="homepage.feature.title">个人特点</Translate>} icon={'ri:map-pin-user-line'}>
-     <BentoGrid className="mx-auto w-full">
-      <div className="py-14">
-        <div className="container mx-auto px-4 md:px-8">
-          <h3 className="text-center text-sm font-semibold text-gray-500">
-            TRUSTED BY LEADING TEAMS
-          </h3>
-          <div className="relative mt-6">
-            <div className="grid grid-cols-2 place-items-center gap-2 md:grid-cols-4 xl:grid-cols-8 xl:gap-4">
-              {companies.map((logo, idx) => (
-                <img
-                  key={idx}
-                  src={`https://cdn.magicui.design/companies/${logo}.svg`}
-                  className="h-10 w-40 px-2 dark:brightness-0 dark:invert"
-                  alt={logo}
-                />
-              ))}
-            </div>
-          </div>
-        </div>
-      </div>
+      <BentoGrid className="mx-auto w-full">
+        {features.map((item, i) => (
+          <BentoGridItem
+            key={i}
+            title={item.title}
+            description={item.description}
+            header={item.header}
+            icon={item.icon}
+            className={cn('p-2 md:p-4', i === 3 || i === 6 ? 'md:col-span-2' : '')}
+          />
+        ))}
+      </BentoGrid>
+
+      {/* <div className="mt-4 grid grid-cols-1 justify-center gap-4 px-0 md:grid-cols-6 md:grid-rows-2 md:px-4">
+        <Skill className="md:col-span-2 md:row-span-2" />
+        <Github className="h-full md:col-span-4 md:row-span-2" />
+      </div> */}
     </Section>
   )
 }
