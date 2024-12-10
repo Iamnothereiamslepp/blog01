766a11f72e827c0862637307ddd89d9ccdfb4a7c
Iamnothereiamslepp <128118699+Iamnothereiamslepp@users.noreply.github.com>
2024-12-10 21:30:06 +0800

    Update index.jsx

--- src/components/landing/logofSection/index.jsx
+++ src/components/landing/logofSection/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './LogoFade.css';  // 引入 CSS 檔案
+import {useTranslation} from '@docusaurus/theme-common';
 
 const LogoFade = () => {
+  const {translate} = useTranslation();  // 使用翻譯 hook
+  
   return (
     <div className="logo-container">
-      <h2>TRUSTED BY LEADING TEAMS</h2>
+      <h2>{translate({message: 'TRUSTEDBYLEADINGTEAMS'})}</h2>
       <div className="logos">
+       <div className="logo-item">
         <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">
           <img src="https://hololivepro.com/wp-content/uploads/2023/03/holostars_hp.png" alt="Google" className="logo fade-in" />
         </a>
