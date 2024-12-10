e78156df912e627d9787df204db7cabed7626b07
Iamnothereiamslepp <128118699+Iamnothereiamslepp@users.noreply.github.com>
2024-12-10 21:06:57 +0800

    Update index.jsx

--- src/components/landing/logofSection/index.jsx
+++ src/components/landing/logofSection/index.jsx
@@ -7,13 +7,13 @@ const LogoFade = () => {
       <h2>TRUSTED BY LEADING TEAMS</h2>
       <div className="logos">
         <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">
-          <img src="google-logo.png" alt="Google" className="logo fade-in" />
+          <img src="https://hololivepro.com/wp-content/uploads/2023/03/holostars_hp.png" alt="Google" className="logo fade-in" />
         </a>
         <a href="https://www.microsoft.com" target="_blank" rel="noopener noreferrer">
-          <img src="microsoft-logo.png" alt="Microsoft" className="logo fade-in" />
+          <img src="https://hololivepro.com/wp-content/uploads/2023/03/officialshop_hp.png" alt="Microsoft" className="logo fade-in" />
         </a>
         <a href="https://www.amazon.com" target="_blank" rel="noopener noreferrer">
-          <img src="amazon-logo.png" alt="Amazon" className="logo fade-in" />
+          <img src="https://hololivepro.com/wp-content/uploads/2023/03/hololive_hp.png" alt="Amazon" className="logo fade-in" />
         </a>
       </div>
     </div>
