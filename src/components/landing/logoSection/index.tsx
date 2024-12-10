9da905b7655394488e7c4d8b708b345e91fd0cee
Iamnothereiamslepp <128118699+Iamnothereiamslepp@users.noreply.github.com>
2024-12-10 20:05:57 +0800

    Update index.tsx

--- src/components/landing/logoSection/index.tsx
+++ src/components/landing/logoSection/index.tsx
@@ -7,13 +7,13 @@ const TrustedTeams = () => {
       <h2>TRUSTED BY LEADING TEAMS</h2>
       <div className="logo-container">
         <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">
-          <img src="google-logo.png" alt="Google" className="logo fade" />
+          <img src="https://hololivepro.com/wp-content/uploads/2023/03/hololive_hp.png" alt="Google" className="logo fade" />
         </a>
         <a href="https://www.microsoft.com" target="_blank" rel="noopener noreferrer">
-          <img src="microsoft-logo.png" alt="Microsoft" className="logo fade" />
+          <img src="https://hololivepro.com/wp-content/uploads/2023/03/hololive_hp.png" alt="Microsoft" className="logo fade" />
         </a>
         <a href="https://www.amazon.com" target="_blank" rel="noopener noreferrer">
-          <img src="amazon-logo.png" alt="Amazon" className="logo fade" />
+          <img src="https://hololivepro.com/wp-content/uploads/2023/03/hololive_hp.png" alt="Amazon" className="logo fade" />
         </a>
       </div>
     </div>
