c1ac6ce99ca78b4e6d510be1679dda140d507640
Iamnothereiamslepp <128118699+Iamnothereiamslepp@users.noreply.github.com>
2024-12-10 18:20:33 +0800

    Update company.tsx

--- data/company.tsx
+++ data/company.tsx
@@ -28,6 +28,8 @@ export function Companies() {
                 />
               ))}
             </div>
+            <div className="pointer-events-none absolute inset-y-0 left-0 h-full w-1/3 bg-gradient-to-r from-white dark:from-black"></div>
+            <div className="pointer-events-none absolute inset-y-0 right-0 h-full w-1/3 bg-gradient-to-l from-white dark:from-black"></div>
           </div>
         </div>
       </div>
