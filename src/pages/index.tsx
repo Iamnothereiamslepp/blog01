58c43cfe3e11c37eda99f7dfda901cd418889544
Iamnothereiamslepp <128118699+Iamnothereiamslepp@users.noreply.github.com>
2024-12-11 20:00:31 +0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import ProjectSection from '../components/landing/ProjectSection'
 import Particles from '../components/magicui/particles'
 import LogoFade from '../components/landing/logofSection'
 import LogoSection from '../components/landing/logopSections'
+import LogoScroll from '../components/landing/logfSection'
 
 export default function Home() {
   const {
@@ -27,6 +28,7 @@ export default function Home() {
             <FeaturesSection />
             <LogoFade />
             <LogoSection />
+            <LogoScroll />
           </div>
           <div
             className="-z-50 absolute inset-0 bg-grid-slate-50 [mask-image:linear-gradient(0deg,#fff,rgba(255,255,255,0.3))] dark:bg-grid-slate-700/25 dark:[mask-image:linear-gradient(0deg,rgba(255,255,255,0.1),rgba(255,255,255,0.5))]"
