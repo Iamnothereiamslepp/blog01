e01c1a97c4b509bff3d797af3478cc28c4781d18
Iamnothereiamslepp <128118699+Iamnothereiamslepp@users.noreply.github.com>
2024-12-12 18:18:42 +0800

    Update TalentCard.tsx

--- src/components/landing/compSection/TalentCard.tsx
+++ src/components/landing/compSection/TalentCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card } from '../components/magicui';
+import { Card } from '../components/magicui/card';
 
 function TalentCard({ image, logo, title }) {
   return (
