5492056c47c991035a82e2b472bf1559703c0631
Iamnothereiamslepp <128118699+Iamnothereiamslepp@users.noreply.github.com>
2024-12-12 17:42:51 +0800

    Update card.tsx

--- src/components/landing/compSection/card.tsx
+++ src/components/landing/compSection/card.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Card, Grid } from '@magic-ui/react';
+import { Card } from '@magic-ui/react';
 
 function TalentCard({ image, logo, title }) {
   return (
     <Card style={{ textAlign: 'center', borderRadius: '10px' }}>
       <Card.Body>
-        <img src={logo} alt="Talent Logo" style={{ width: '50px', marginBottom: '10px' }} />
+        <img src={logo} alt="Logo" style={{ width: '50px', marginBottom: '10px' }} />
         <img src={image} alt="Talent" style={{ width: '100%', borderRadius: '5px' }} />
         <h3 style={{ marginTop: '10px' }}>{title}</h3>
       </Card.Body>
