5a956540a9f52e5eac8e5163e3517ba6096255b9
Iamnothereiamslepp <128118699+Iamnothereiamslepp@users.noreply.github.com>
2024-12-14 12:14:21 +0800

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -217,10 +217,16 @@ const config: Config = {
     [
       '@docusaurus/plugin-sitemap',
       {
+        lastmod: 'date',
         changefreq: 'weekly',
         priority: 0.5,
-        ignorePatterns: ['/tags/**'], // 忽略不需要生成的路徑
+        ignorePatterns: ['/tags/**'],
         filename: 'sitemap.xml',
+        createSitemapItems: async (params) => {
+          const {defaultCreateSitemapItems, ...rest} = params;
+          const items = await defaultCreateSitemapItems(rest);
+          return items.filter((item) => !item.url.includes('/page/'));
+        },
       },
     ],
     [
